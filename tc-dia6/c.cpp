01b3565a563a942e9c1798e76604d4dae28d8bba
nazanecchi <nazanecchi.cer@gmail.com>
2024-07-22 15:32:43 -0300

    pepa c

--- tc-dia6/c.cpp
+++ tc-dia6/c.cpp
@@ -5,24 +5,31 @@ using namespace std;
 #define fori(i, n) for(int i = n - 1; i <= 0; i--)
 #define mos(v) forn(auto i : v) cout << i << " ";
 
+int calculo(int r, int i){
+    int primero = r/(i*2);
+    int segundo = i;
+    int tercero = r%i + 1;
+    int cuarto = ((r/i) % 2);
+    int resr = primero * segundo + tercero * cuarto;
+    //cout << primero << " " << segundo << " " << tercero << " " << cuarto << " " << endl;
+    resr = r + 1 - resr;
+    return resr;
+}
+
 int main(){
-    freopen("input.txt", "r", stdin);
+    //freopen("input.txt", "r", stdin);
     int t;
     cin >> t;
     forn(j,t){
         int r,l, prod = 1, i=0, minl = 1E9;
         cin >> l >> r;
         while(prod <= r){
-            int auxr = (((r+prod)/(prod*2))-1);
-            int auxl = (((l+prod)/(prod*2))-1);
-            if(auxl < 0) auxl = 0;
-            if(auxr < 0) auxr = 0;
-            int resr = auxr *prod + (r%prod) * ((r/prod) % 2);
-            int resl = auxl *prod + (l%prod) * ((l/prod) % 2);
+            int resl = calculo(l-1, prod);
+            int resr = calculo(r, prod);
             //cout << resl << " " << resr << endl;
-            minl = min(minl, resr - resl);
             prod *= 2;
             i++;
+            minl = min(minl, resr - resl);
         }
         cout << minl << endl;
     }
