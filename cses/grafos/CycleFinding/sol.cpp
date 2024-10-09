bab0cd90d7f22549166cc0e921ae4785752058c7
lautarotetamusa <lautarotetamusa@gmail.com>
2024-10-09 14:57:10 -0300

    add: cses grafos CycleFinding

--- cses/grafos/CycleFinding/sol.cpp
+++ cses/grafos/CycleFinding/sol.cpp
@@ -1,87 +1,64 @@
 #include <algorithm>
 #include <bits/stdc++.h>
+#include <climits>
 using namespace std;
 #define forn(i, n) for(int i = 0; i < n; i++)
 #define DBG(x) cerr << #x << ": " << x << endl;
 
 typedef long long ll;
-const ll MAXN = 9999999999LL;
+const ll INF = LONG_LONG_MAX;
 
 /* Solucion: 
  * Bellman-Ford
  * */
-
-vector<bool> visited;
-
-bool dfs(int nodo, vector<vector<int>> &adj, unordered_set<int> &cycle, stack<int> &path){
-    if(visited[nodo]) return true;
-    visited[nodo] = true;
-    path.push(nodo);
-
-    for(int b : adj[nodo]){
-        if (!visited[b] && cycle.find(b) != cycle.end()){
-            if(dfs(b, adj, cycle, path)) return true;
-        }
-        if (visited[b]) return true;
-    }
-    path.pop();
-    return false;
-}
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     int n, m;
     cin >> n >> m;
-    vector<tuple<int, int, ll>> edges(m);
-    vector<vector<int>> adj(n);
+    vector<tuple<int, int, ll>> edges;
+    vector<ll> distance(n, 0);
+    vector<int> parent(n, -1);
 
     forn(i, m){
         int a, b;
         ll c;
         cin >> a >> b >> c; a--; b--;
         edges.push_back({a, b, c});
-        adj[a].push_back(b);
     }
 
-    vector<ll> distance(n, MAXN);
+    int x = -1; // Perteneciente a un ciclo negativo, o alzancable desde él
     distance[0] = 0;
-
-    forn(i, n-1){
+    forn(i, n){
+        x = -1;
         for (auto [a, b, w] : edges) {
-            distance[b] = min(distance[b], distance[a]+w);
+            if (distance[a]+w < distance[b]) {
+                distance[b] = max(-INF, distance[a]+w);
+                parent[b] = a;
+                x = b;
+            }
         }
     }
 
-    // Guardamos los nodos que forman parte de un ciclo positivo
-    int cycle_start = 1e6;
-    unordered_set<int> cycle;
-    for (auto [a, b, w] : edges) {
-        if (distance[a]+w < distance[b]) {
-            cycle.insert(a);
-            cycle_start = min(cycle_start, a);
-        }
-        distance[b] = min(distance[b], distance[a]+w);
+    if (x == -1){
+        cout << "NO\n"; 
+        return 0;
     }
 
-    stack<int> path;
-    visited.assign(n, false);
-    bool have_path = dfs(cycle_start, adj, cycle, path);
-    path.push(cycle_start);
-
-    if (have_path){
-        cout << "YES\n";
-        vector<int> P;
-        while(!path.empty()){
-            int p = path.top(); path.pop();
-            P.push_back(p+1);
-        }
-        reverse(P.begin(), P.end());
-        for (int p : P) cout << p << ' '; 
-        cout << '\n';
-    }else{
-        cout << "NO\n";
+    forn(i, n) {
+        x = parent[x];
     }
+    cout << "YES\n";
+    vector<int> cycle;
+    for (int v = x;; v = parent[v]) {
+        cycle.push_back(v);
+        if (v == x && cycle.size() > 1) break;
+    }
+    reverse(cycle.begin(), cycle.end());
+
+    for (int c : cycle) cout << c+1 << ' '; 
+    cout << '\n';
  
     return 0;
 }
