3f7620bc20ee6940712d0393ea3a4e5e99bc958c
nazanecchithorque <nazareno@thorque.com.ar>
2025-08-22 15:09:48 -0300

    add: mincut, max matching and recovery path

--- cses/grafos/DistinctRoutes/a.cpp
+++ cses/grafos/DistinctRoutes/a.cpp
@@ -2,16 +2,18 @@
 #define forr(i, a, b) for (int i = (a); i < (b); i++)
 #define forn(i, n) forr(i, 0, n)
 #define dforn(i, n) for (int i = (n) - 1; i >= 0; i--)
+#include <vector>
+#include <cstring>
+using namespace std;
+typedef long long ll;
 #define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
+#define zero(v) memset(v, 0, sizeof(v))
 #define pb push_back
 #define sz(v) ((int)v.size())
 #define INF 1e18
-
-using namespace std;
-typedef long long ll;
-
+ 
 // ojo con el max
-const int MAX = 501;
+const int MAX = 1100;
 // Corte minimo: vertices con dist[v]>=0 (del lado de src) VS.  dist[v]==-1 (del lado del dst)
 // Para el caso de la red de Bipartite Matching (Sean V1 y V2 los conjuntos mas proximos a src y dst respectivamente):
 // Reconstruir matching: para todo v1 en V1 ver las aristas a vertices de V2 con it->f>0, es arista del Matching
@@ -22,24 +24,20 @@ const int MAX = 501;
 int nodes, src, dst;
 int dist[MAX], q[MAX], work[MAX];
 struct Edge {
-    int to;
-    int rev; // indice en en la red residual
-    ll f; // flow
-    ll cap; // capacity
+    int to, rev;
+    ll f, cap;
     Edge(int to, int rev, ll f, ll cap) : to(to), rev(rev), f(f), cap(cap) {}
 };
-
 vector<Edge> G[MAX];
 void addEdge(int s, int t, ll cap){
     G[s].pb(Edge(t, sz(G[t]), 0, cap));
     G[t].pb(Edge(s, sz(G[s])-1, 0, 0));
 }
-
 bool dinic_bfs(){
     fill(dist, dist+nodes, -1), dist[src]=0;
     int qt=0; q[qt++]=src;
     for(int qh=0; qh<qt; qh++){
-        int u = q[qh];
+        int u =q[qh];
         forall(e, G[u]){
             int v=e->to;
             if(dist[v]<0 && e->f < e->cap)
@@ -49,83 +47,82 @@ bool dinic_bfs(){
     return dist[dst]>=0;
 }
 ll dinic_dfs(int u, ll f){
-    if(u == dst) return f;
+    if(u==dst) return f;
     for(int &i=work[u]; i<sz(G[u]); i++){
         Edge &e = G[u][i];
-        if(e.cap <= e.f) continue;
-        int v = e.to;
+        if(e.cap<=e.f) continue;
+        int v=e.to;
         if(dist[v]==dist[u]+1){
-                ll df = dinic_dfs(v, min(f, e.cap-e.f));
+                ll df=dinic_dfs(v, min(f, e.cap-e.f));
                 if(df>0){
-                    e.f+=df, G[v][e.rev].f-= df;
-                    return df;
-                }
+                        e.f+=df, G[v][e.rev].f-= df;
+                        return df;  }
         }
     }
     return 0;
 }
-
 ll maxFlow(int _src, int _dst){
     src=_src, dst=_dst;
-    ll result = 0;
+    ll result=0;
     while(dinic_bfs()){
         fill(work, work+nodes, 0);
         while(ll delta=dinic_dfs(src,INF))
-            result += delta;
+            result+=delta;
     }
     // todos los nodos con dist[v]!=-1 vs los que tienen dist[v]==-1 forman el min-cut
-    return result; 
-}
-
-bool vis[501][501];
-void corteMinimo(){
-    forn(i, nodes){
-        if(dist[i] >= 0){
-            for(auto j : G[i]){
-                if(dist[j.to] == -1 && !vis[i][j.to]){
-                    vis[i][j.to] = true;
-                    cout << i+1 << " " << j.to+1 << endl;
-                }
-            }
-        }
-    }
-}
-
-int capa1, capa2, aristas;
-void matchingMaximo(){
-    forr(i,1, capa1+1){
-        for(auto j : G[i]){
-            if(j.f > 0){
-                cout << i << " " << j.to - capa1 << endl;
-            }
-        }
-    }
-}
-vector<int> path;
-void recrearUnCamino(int i){ //solo te recrea un camino del flujo
-    path.push_back(i);
+    return result; }
+ 
+vector<int> aux;
+bool visitado[504][504];
+void dfs(int i){
     if(i == nodes-1)return;
     for(auto j: G[i]){
-        if(j.f>0 && !vis[i][j.to]){
-            vis[i][j.to] = true;
-            path.push_back(j.to+1);
-            recrearUnCamino(j.to);
+        if(j.f>0 && !visitado[i][j.to]){
+            visitado[i][j.to] = true;
+            aux.push_back(j.to+1);
+            dfs(j.to);
             break;
         }
     }
 }
-
-
+ 
+#ifdef EBUG
+// local
+#else
+// judge
+#endif
+ 
+bool dp[501][501];
 int main() {
     // Example usage
-    int m;
-    cin >> nodes >> m;
+    #ifdef EBUG
+    freopen("input.txt", "r", stdin);
+    #endif
+    int n, m;
+    cin >> n >> m;
+    nodes = n;
     forn(i, m){
-        int a, b, c;
-        cin >> a >> b >> c;
-        addEdge(a-1, b-1, (ll)c);
+        int a , b;
+        cin >> a >> b;
+        addEdge(a-1,b-1,(ll)1);
     }
     ll flow = maxFlow(0, nodes-1);
-    cout << flow << endl;
+    vector<vector<int>> v;
+    for(auto j: G[0]){
+        if(j.f>0){
+            aux.clear();
+            aux.push_back(1);
+            aux.push_back(j.to+1);
+            dfs(j.to);
+            v.push_back(aux);
+        }
+    }
+    cout << v.size() << endl;
+    forn(i, v.size()){
+        cout << v[i].size() << endl;
+        forn(j, v[i].size()){
+            cout << v[i][j] << " ";
+        } cout << endl;
+    }
     return 0;
 }
