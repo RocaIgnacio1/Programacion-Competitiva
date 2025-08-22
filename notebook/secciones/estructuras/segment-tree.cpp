6eec7720a690eb17135f744d523342e269bf154b
nazanecchithorque <nazareno@thorque.com.ar>
2025-08-21 21:53:53 -0300

    add: segment tree lazy

--- notebook/secciones/estructuras/segment-tree.cpp
+++ notebook/secciones/estructuras/segment-tree.cpp
@@ -38,14 +38,59 @@ struct RMQ{
     }
 }mx;
 
+//Dado un arreglo y una operacion asociativa con neutro, get(i, j) opera sobre el rango [i, j).
+typedef int Elem;//Elem de los elementos del arreglo
+typedef int Alt;//Elem de la alteracion
+#define operacion(x,y) x+y
+const Elem eneutro=0; const Alt aneutro=0;
+#define MAXN 100000
+struct RMQ{
+	int sz;
+	Elem t[4*MAXN];
+	Alt dirty[4*MAXN];//las alteraciones pueden ser de distinto Elem
+	Elem &operator[](int p){return t[sz+p];}
+	void init(int n){//O(nlgn)
+		sz = 1 << (32-__builtin_clz(n));
+		forn(i, 2*sz) t[i]=eneutro;
+		forn(i, 2*sz) dirty[i]=aneutro;
+	}
+    void updall(){
+        dforn(i, sz) t[i] = operacion(t[2*i], t[2*i+1]);
+    }
+	void push(int n, int a, int b){//propaga el dirty a sus hijos
+		if(dirty[n]!=aneutro){
+			t[n]+=dirty[n]*(b-a);//altera el nodo 
+			if(n<sz){
+				dirty[2*n]+=dirty[n];
+				dirty[2*n+1]+=dirty[n];
+			}
+			dirty[n]=aneutro;
+		}
+	}
+	Elem get(int i, int j, int n, int a, int b){//O(lgn)
+		if(j<=a || i>=b) return eneutro;
+		push(n, a, b);//corrige el valor antes de usarlo
+		if(i<=a && b<=j) return t[n];
+		int c=(a+b)/2;
+		return operacion(get(i, j, 2*n, a, c), get(i, j, 2*n+1, c, b));
+	}
+	Elem get(int i, int j){return get(i,j,1,0,sz);}
+	//altera los valores en [i, j) con una alteracion de val
+	void alterar(Alt val, int i, int j, int n, int a, int b){//O(lgn)
+		push(n, a, b);
+		if(j<=a || i>=b) return;
+		if(i<=a && b<=j){
+			dirty[n]+=val; //ver esto gordo
+			push(n, a, b);
+			return;
+		}
+		int c=(a+b)/2;
+		alterar(val, i, j, 2*n, a, c), alterar(val, i, j, 2*n+1, c, b);
+		t[n]=operacion(t[2*n], t[2*n+1]);//por esto es el push de arriba
+	}
+	void alterar(Alt val, int i, int j){alterar(val,i,j,1,0,sz);}
+}rmq;
+
+
 mx.init(v.size()); forn(i, v.size()) mx[i] = v[i]; mx.updall();
 
-ll searchST(int v, int tl, int tr,  int m){ //buscar el primer elemento mayor a un numero
-   if( tl == tr){
-       return tl;
-   }
-   int tm = (tl + tr)/2;
-   if(t[2*v] >= m) return searchST(2*v, tl, tm, m);
-   return searchST(2*v+1, tm+1, tr, m);
-
-}
