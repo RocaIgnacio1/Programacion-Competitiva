1bc00c84aab391caf3c0fbfcd314e328ea0b3aca
nazanecchithorque <nazareno@thorque.com.ar>
2025-08-31 20:35:51 -0300

    fix: c tap 2025

--- simulaciones/TAPs/tap2025/c.cpp
+++ simulaciones/TAPs/tap2025/c.cpp
@@ -10,32 +10,15 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
 
-#define MAXN 300000
-#define operacion(x, y) op(x, y)
-#define neutro {-1, 0, 0}
-#define tipo tuple<int, int, int> // {ganas o perdes, cacheTotal, acumx, acumy}
-
 int main() {
     HOLA
-    int n;
-    cin >> n;
-    vector<int> v;
-    vector<int> cant(n, 0);
-    forn(i, n){
-        int k;
-        cin >> k;
-        v.push_back(k);
-    }
-
+    int n;cin >> n;
+    vector<int> v(n), cant(n, 0);
+    forn(i, n) cin >> v[i];
     reverse(v.begin(), v.end());
-    forn(i, n){
-        //cout << i << " " << v[i]-1 << " " << (i+n-(v[i]-1))%n << endl;
-        cant[(i+n-(v[i]-1))%n]++;
-    }
+    forn(i, n) cant[(i+n-(v[i]-1))%n]++;
     ll acum = 0;
-    forn(i, n){
-        acum += (pow(cant[i], 2));
-    }
+    forn(i, n)acum += (cant[i] * cant[i]);
     cout << acum << endl;
     return 0;
 }
